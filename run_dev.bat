d8da1c56f0cceb5c00c781b95f254cd28fc2459d
Daniel Košč <daniel.kosc@gmail.com>
2025-12-02 12:22:46 +0100

    Fix: Project name can contain uppercase

--- run_dev.bat
+++ run_dev.bat
@@ -57,6 +57,9 @@ REM Get repository name
 for %%I in ("%CD%") do set "DIRNAME=%%~nxI"
 set "REPO_NAME=%DIRNAME%"
 set "REPO_NAME=%REPO_NAME:workflows-=%"
+for %%a in ("A=a" "B=b" "C=c" "D=d" "E=e" "F=f" "G=g" "H=h" "I=i" "J=j" "K=k" "L=l" "M=m" "N=n" "O=o" "P=p" "Q=q" "R=r" "S=s" "T=t" "U=u" "V=v" "W=w" "X=x" "Y=y" "Z=z") do (
+    call set "REPO_NAME=%%REPO_NAME:%%~a%%"
+)
 set "APP_NAME=workflows-app-%REPO_NAME%"
 
 echo Preparing %APP_NAME%...
